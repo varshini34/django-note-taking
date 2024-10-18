6f92ef7213158def96177ff71713021d260ac3df
Avanthika Varshini <avanthikavarshini996@gmail.com>
2024-10-18 08:31:56 +0530

    file rename

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -70,7 +70,7 @@ ROOT_URLCONF = 'learning_log.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
